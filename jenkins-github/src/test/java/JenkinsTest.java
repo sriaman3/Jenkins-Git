53b7ada20f3bcebef327de539a16cba36e1d8621
Aman Srivastava <sriaman3@gmail.com>
2021-05-30 07:02:43 +0530

    test git commit

---
+++ jenkins-github/src/test/java/JenkinsTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class JenkinsTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void init() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://demoqa.com/text-box");
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		
+		//driver.get("https://www.seleniumeasy.com/test/table-pagination-demo.html");
+		driver.findElement(By.xpath("//*[@id='userName']")).sendKeys("Aman");
+		driver.findElement(By.xpath("//*[@id='userEmail']")).sendKeys("Aman@gmail.com");
+		driver.findElement(By.xpath("//*[@id='currentAddress']")).sendKeys("Delhi");
+		driver.findElement(By.xpath("//*[@id='permanentAddress']")).sendKeys("Chandigarh");
+	}
+	
+	@Test
+	public void submit() {
+		driver.findElement(By.xpath("//*[@id='submit1']")).click();
+	}
+
+}
